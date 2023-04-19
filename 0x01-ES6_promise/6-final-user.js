8d0ba764936d7508983873a7f05c7b68b257d8e7
Mkiige <mkiige77@gmail.com>
2023-04-19 22:49:35 +0300

    correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,16 @@
-/* eslint-disable import/named */
-// eslint-disable-next-line import/named
 import { signUpUser } from './4-user-promise';
 import { uploadPhoto } from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => result.map((res) => (
-      {
-        status: res.status,
-        value: res.status === 'fulfilled' ? res.value : res.reason,
-      }
-    )));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ]).then((results) => {
+    return results.map((result) => {
+      return {
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+      };
+    });
+  });
 }
